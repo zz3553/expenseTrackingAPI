7119eac76bca46d917af29c544d0805a76fe8787
zz3553 <zz3553@g.rit.edu>
2025-03-17 17:52:16 -0400

    Added in delete functionality

--- routers/expenses.py
+++ routers/expenses.py
@@ -22,10 +22,22 @@ def read_expense(expense_id: int, database: Session = Depends(db.get_db)):
         raise HTTPException(status_code=404, detail="Expense not found")
     return expense
 
-@router.put('/{expense_id}', response_model=schemas.ExpenseResponse)
+@router.put("/{expense_id}", response_model=schemas.ExpenseResponse)
 def update_expense(
         expense_id: int,
         updates: schemas.ExpenseUpdate,
         database: Session = Depends(db.get_db)
 ):
     return crud.update_expense(database, expense_id, updates)
+
+@router.delete("/{expense_id}", response_model=schemas.ExpenseResponse)
+def delete_expense(
+        expense_id: int,
+        database: Session = Depends(db.get_db)
+):
+    expense = crud.get_expense(database, expense_id)
+
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+
+    return crud.delete_expense(database, expense_id)
