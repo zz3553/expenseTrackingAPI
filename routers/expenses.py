72ad8790705f7b538353884fd8e3c6be2ecba4f1
zz3553 <zz3553@g.rit.edu>
2025-06-17 15:56:37 -0400

    Separated out crud operations for expenses

--- routers/expenses.py
+++ routers/expenses.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from typing import List
 
-import crud, schemas, db
+import expense_crud, schemas, db
 import logging
 
 logger = logging.getLogger(__name__)
@@ -17,7 +17,7 @@ router = APIRouter(prefix="/expenses", tags=["expenses"])
 
 @router.post("/", response_model=schemas.ExpenseResponse)
 def create_expense(expense: schemas.ExpenseCreate, database: Session = Depends(db.get_db)):
-    return crud.create_expense(database, expense)
+    return expense_crud.create_expense(database, expense)
 
 
 @router.get("/", response_model=List[schemas.ExpenseResponse])
@@ -30,7 +30,7 @@ def get_expenses(
     start_date = datetime.strptime(start_date, "%m/%d/%Y") if start_date else datetime.min
     end_date = datetime.strptime(end_date, "%m/%d/%Y").replace(hour=23, minute=59, second=59) if end_date else datetime.max
 
-    return crud.get_last_x_expenses(
+    return expense_crud.get_last_x_expenses(
         database,
         limit=limit,
         start_date=start_date,
@@ -43,12 +43,12 @@ def get_expenses_filtered_by_type(
         category: str = Query(...),
         database: Session = Depends(db.get_db)
 ):
-    return crud.get_expense_by_category(database, category)
+    return expense_crud.get_expense_by_category(database, category)
 
 
 @router.get("/{expense_id}", response_model=schemas.ExpenseResponse)
 def get_expense_with_id(expense_id: int, database: Session = Depends(db.get_db)):
-    expense = crud.get_expense(database, expense_id)
+    expense = expense_crud.get_expense(database, expense_id)
     if not expense:
         raise HTTPException(status_code=404, detail="Expense not found")
     return expense
@@ -60,7 +60,7 @@ def update_expense(
         updates: schemas.ExpenseUpdate,
         database: Session = Depends(db.get_db)
 ):
-    return crud.update_expense(database, expense_id, updates)
+    return expense_crud.update_expense(database, expense_id, updates)
 
 
 @router.delete("/{expense_id}", response_model=schemas.ExpenseResponse)
@@ -68,9 +68,9 @@ def delete_expense(
         expense_id: int,
         database: Session = Depends(db.get_db)
 ):
-    expense = crud.get_expense(database, expense_id)
+    expense = expense_crud.get_expense(database, expense_id)
 
     if not expense:
         raise HTTPException(status_code=404, detail="Expense not found")
 
-    return crud.delete_expense(database, expense_id)
+    return expense_crud.delete_expense(database, expense_id)
